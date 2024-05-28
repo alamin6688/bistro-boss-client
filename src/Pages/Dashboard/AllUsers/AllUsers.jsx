bfa18d747aaebe9e1af3dddf0a44df9c266c245e
alamin6688 <alamin.kzs06@gmail.com>
2024-05-28 22:57:46 +0600

    Admin Status role update funtionalities added

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -14,7 +14,19 @@ const AllUsers = () => {
   });
 
   const handleMakeAdmin = user =>{
-    
+    axiosSecure.patch(`/users/admin/${user._id}`)
+    .then(res =>{
+      if(res.data.modifiedCount > 0){
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
   }
 
   const handleDeleteUser = user =>{
@@ -31,6 +43,7 @@ const AllUsers = () => {
       if(result.isConfirmed){
         axiosSecure.delete(`/users/${user._id}`)
         .then(res =>{
+          console.log(res.data)
           if (res.data.deletedCount > 0){
             refetch();
             Swal.fire({
@@ -69,16 +82,15 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                  {user.role === "admin" ? (
-                    "Admin"
-                  ) : (
-                    <button
-                      onClick={() => handleMakeAdmin(user)}
-                      className="btn btn-lg bg-orange-500"
-                    >
-                      <FaUsers className="text-white text-2xl"/>
-                    </button>
-                  )}
+                  {
+                    user.role === "admin" ? ("Admin") : 
+                    ( <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-lg bg-orange-500">
+                        <FaUsers className="text-white text-2xl"/>
+                      </button>
+                    )
+                  }
                 </td>
                 <td>
                   <button
